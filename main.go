9b529940d85106cb1e38214cb74775f12b2b1f01
admin <2078561544@qq.com>
2023-06-16 14:11:22 +0800

    change to 80

--- main.go
+++ main.go
@@ -9,26 +9,26 @@ import (
 )
 
 func main() {
-	http。HandleFunc("/sysconf", api。SysConf)
+	http.HandleFunc("/sysconf", api.SysConf)
 
-	http。HandleFunc("/sydney/", api。Sydney)
+	http.HandleFunc("/sydney/", api.Sydney)
 
-	http。HandleFunc("/web/", api。WebStatic)
+	http.HandleFunc("/web/", api.WebStatic)
 
-	http。HandleFunc("/", api。Index)
+	http.HandleFunc("/", api.Index)
 
-	port := os。Getenv("PORT")
+	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8080"
+		port = "80"
 	}
 	addr := ":" + port
 
-	log。Println("Starting BingAI Proxy At " + addr)
+	log.Println("Starting BingAI Proxy At " + addr)
 
-	srv := &http。Server{
+	srv := &http.Server{
 		Addr:         addr,
-		WriteTimeout: 15 * time。Second,
-		ReadTimeout:  15 * time。Second,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
 	}
-	log。Fatal(srv。ListenAndServe())
+	log.Fatal(srv.ListenAndServe())
 }
